f2cc45be22be9e93134f9bdf564131c8fa81c1f6
chirag110 <chiragguglani@yahoo.in>
2024-12-20 02:23:46 +0530

    Time: 1 ms (100.00%), Space: 57.9 MB (58.64%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+    int minPrice = int.MaxValue;
+    int maxProfit = 0;
+
+    foreach (int price in prices) {
+        if (price < minPrice) {
+            minPrice = price;
+        }
+        int profit = price - minPrice;
+        if (profit > maxProfit) {
+            maxProfit = profit;
+        }
+    }
+    
+    return maxProfit;
+}
+
+}
